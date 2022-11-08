0e0c637216d5c2ee82358ab55d2da17a6dcff50f
Jeremiah Griffin <jeremiahgriffin@protonmail.com>
2022-11-08 08:44:37 -0500

    added export card

--- src/app/automate/automate.module.ts
+++ src/app/automate/automate.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import {AutomateComponent} from "./automate.component";
 import {MatCardModule} from "@angular/material/card";
 import { ImportCardComponent } from './import-card/import-card.component';
+import { ExportCardComponent } from './export-card/export-card.component';
 
 @NgModule({
   declarations: [
     AutomateComponent,
     ImportCardComponent,
+    ExportCardComponent,
   ],
   imports: [
     CommonModule,
